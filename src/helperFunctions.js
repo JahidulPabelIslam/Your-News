759eb5f5353f3af2da3705bbcef7ae41a84f0a4b
Jahidul Pabel Islam <jahidul.pabel.islam@hotmail.com>
2016-12-06 14:26:41 +0000

    Better Date & Time in responses.
    Fixed minor errors.
    Removed repetitive code.

--- src/helperFunctions.js
+++ src/helperFunctions.js
@@ -12,82 +12,98 @@ var textHelper = (function () {
         getDate: function (fixture) {
             var gameDate = new Date(fixture.date),
                 currentDate = new Date(),
-                timeString = " ";
+                outputSpeech = " ";
+
+            var dateDifference;
 
             //if game is today
             if (gameDate.getDate() == currentDate.getDate() && gameDate.getMonth() == currentDate.getMonth() && gameDate.getFullYear() == currentDate.getFullYear()) {
+
                 if (fixture.status == "FINISHED") {
                     if (fixture.result.penaltyShootout !== undefined) {
                         gameDate.setHours(gameDate.getHours() + 2, gameDate.getMinutes() + 45);
-                    } else if (fixture.result.extraTime !== undefined) {
+                    }
+                    else if (fixture.result.extraTime !== undefined) {
                         gameDate.setHours(gameDate.getHours() + 2, gameDate.getMinutes() + 25);
-                    } else {
+                    }
+                    else {
                         gameDate.setHours(gameDate.getHours() + 1, gameDate.getMinutes() + 45);
                     }
                 }
 
-                var hoursDifference = gameDate.getHours() - currentDate.getHours();
+                dateDifference = gameDate - currentDate;
 
-                if (hoursDifference < 0) {
-                    hoursDifference += 2;
-                }
+                var hoursDifference = parseInt((dateDifference / (1000 * 60 * 60)) % 24);
+                var minutesDifference = parseInt((dateDifference / 1000 / 60) % 60);
 
                 if (hoursDifference == 0) {
 
-                    timeString = " today ";
-                    var minuteDifference = gameDate.getMinutes() - currentDate.getMinutes();
-
-                    if (minuteDifference > 1) timeString += "in " + minuteDifference + " minutes";
-                    else if (minuteDifference < 1) {
-                        minuteDifference = currentDate.getMinutes() - gameDate.getMinutes();
-                        timeString += minuteDifference + " minutes ago";
+                    if (minutesDifference > 1) {
+                        outputSpeech += "in " + minutesDifference + " minutes";
                     }
-                    else timeString += minuteDifference + " minute ago";
+                    else if (minutesDifference < -1) {
+                        outputSpeech += Math.abs(minutesDifference) + " minutes ago";
+                    }
+                    else if (minutesDifference == -1) {
+                        outputSpeech += "one minute ago";
+                    }
+                    else {
+                        outputSpeech += "in one minute";
+                    }
+                }
+                else if (hoursDifference > 0) {
+                    outputSpeech += "in " + hoursDifference + " hour";
+                    if (hoursDifference > 1) {
+                        outputSpeech += "s";
+                    }
 
-                    return timeString;
+                    outputSpeech += " " + minutesDifference + " minute";
+                    if (minutesDifference > 1) outputSpeech += "s";
+                }
+                else {
+                    outputSpeech += Math.abs(hoursDifference) + " hour";
+                    if (Math.abs(hoursDifference) > 1) {
+                        outputSpeech += "s";
+                    }
 
-                } else if (hoursDifference > 0) {
-                    timeString = " today in " + hoursDifference + " hour";
-                    if (hoursDifference > 1) timeString += "s";
-                    return timeString;
-                } else {
-                    timeString = " today " + Math.abs(hoursDifference) + " hour";
-                    if (Math.abs(hoursDifference) > 1) timeString += "s";
-                    return timeString + " ago";
+                    outputSpeech += " " + Math.abs(minutesDifference) + " minute";
+                    if (Math.abs(minutesDifference) > 1) outputSpeech += "s";
 
+                    outputSpeech += " ago";
                 }
             }
-            //if its this month
-            else if (gameDate.getMonth() == currentDate.getMonth() && gameDate.getFullYear() == currentDate.getFullYear()) {
+            else {
+                //set the date to same time to get accurate days difference
+                currentDate.setHours(0, 0, 0);
+                dateDifference = gameDate - currentDate;
 
-                var daysDifference = gameDate.getDate() - currentDate.getDate();
+                var daysDifference = Math.floor(dateDifference / (1000 * 60 * 60 * 24));
 
-                if (daysDifference > 1) timeString += "in " + daysDifference + " days on " + days[gameDate.getDay()] + " the " + gameDate.getDate() + " of " + months[gameDate.getMonth()];
-                else if (daysDifference == 1) timeString += "tommorrow";
-                else if (daysDifference == -1) timeString += "yesterday";
-                else timeString += Math.abs(daysDifference) + " days ago on " + days[gameDate.getDay()] + " the " + gameDate.getDate() + " of " + months[gameDate.getMonth()];
-
-                return timeString;
-            } else {
-                return " on " + days[gameDate.getDay()] + " the " + gameDate.getDate() + " of " + months[gameDate.getMonth()];
+                if (daysDifference > 1) {
+                    outputSpeech += "in " + daysDifference + " days on " + days[gameDate.getDay()] + " the " + gameDate.getDate() + " of " + months[gameDate.getMonth()];
+                }
+                else if (daysDifference == 1) {
+                    outputSpeech += "tomorrow";
+                }
+                else if (daysDifference == -1) {
+                    outputSpeech += "yesterday";
+                }
+                else {
+                    outputSpeech += Math.abs(daysDifference) + " days ago on " + days[gameDate.getDay()] + " the " + gameDate.getDate() + " of " + months[gameDate.getMonth()];
+                }
             }
+            return outputSpeech;
         },
 
-        completeHelp: 'Here\'s some things you can say,'
-        + ' add chelsea fc.'
-        + ' delete chelsea fc.'
-        + ' latest score for my teams.'
-        + ' latest score for chelsea fc.'
-        + ' latest news for my teams.'
-        + ' latest news for chelsea fc.'
-        + ' next fixture for my teams.'
-        + ' next fixtures for chelsea fc.'
-        + ' reset my teams.'
-        + ' repeat.'
-        + ' and exit. What would you like?',
+        completeHelp: 'Here\'s some things you can say, add chelsea fc.'
+        + ' delete chelsea fc. latest score for my teams.'
+        + ' latest score for chelsea fc. latest news for my teams.'
+        + ' latest news for chelsea fc. next fixture for my teams.'
+        + ' next fixtures for chelsea fc. reset my teams.'
+        + ' stop. What would you like?',
 
-        nextHelp: 'You can add a team, delete a team, get the score for your team\'s or a team, get the latest news for your team\'s or a team, get the next fixture for your team\'s or a team, or say help. What would you like?'
-
+        nextHelp: "You can add a team or delete a team from your favourites list. Or get the score, "
+        + "latest news, or next fixture for a team, or get help. What would you like to do or know?"
     };
 
 })();
